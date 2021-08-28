886e09f548d244db82b76b2d05f6d0e31b3d3134
OnoprienkoArtem <o.artem.e@gmail.com>
2021-08-28 23:13:13 +0300

    add create method and user repository

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('register')
   register() {
     return 'this register route post';
