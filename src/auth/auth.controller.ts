ab82f7c9a4b914acf19c159598191d743b756c83
OnoprienkoArtem <o.artem.e@gmail.com>
2021-08-28 23:34:03 +0300

    done register user and encrypt password by bcrypt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register() {
-    return 'this register route post';
+  async register(@Body() body: any) {
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.authService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
   }
 }
