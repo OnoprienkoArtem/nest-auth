5a2a84f264dcd6dccdd980fb06bfa469e15bb8ce
OnoprienkoArtem <o.artem.e@gmail.com>
2021-08-29 22:14:49 +0300

    add register dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body: any) {
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Password do not match.');
+    }
+
     const hashed = await bcrypt.hash(body.password, 12);
 
     return this.authService.create({
