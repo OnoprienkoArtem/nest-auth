6fceb889d4c7731b9ba13c194fa450e938095762
OnoprienkoArtem <o.artem.e@gmail.com>
2021-09-02 23:01:56 +0300

    create token and add to db email and token for reset

--- src/reset/reset.controller.ts
+++ src/reset/reset.controller.ts
@@ -1,10 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ResetService } from './reset.service';
 
 @Controller()
 export class ResetController {
+  constructor(private resetService: ResetService) {}
 
   @Post('forgot')
   async forgot(@Body('email') email: string) {
+    const token = Math.random().toString(20).substr(2, 12);
 
+    await this.resetService.create({ email, token });
+
+    return { message: 'Success' };
   }
 }
--- src/reset/reset.service.ts
+++ src/reset/reset.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ResetEntity } from './models/reset.entity';
+import { Reset } from './models/reset.interface';
 
 @Injectable()
-export class ResetService {}
+export class ResetService {
+  constructor(
+    @InjectRepository(ResetEntity)
+    private readonly resetRepository: Repository<ResetEntity>,
+  ) {}
+
+  async create(reset: Reset): Promise<Reset> {
+    return this.resetRepository.save(reset);
+  }
+}
