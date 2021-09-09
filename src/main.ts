e46ea3e30a65475cc45b12f382894db08a072282
OnoprienkoArtem <o.artem.e@gmail.com>
2021-09-09 23:26:59 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors();
+
   await app.listen(8000);
 }
 
