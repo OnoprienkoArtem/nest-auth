df97a151ded9c8d4fe5d7b56db03b40a09f1e24d
OnoprienkoArtem <o.artem.e@gmail.com>
2021-09-11 23:55:16 +0300

    instal cors package

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors();
+  app.use(
+    cors({
+      origin: 'http://localhost:4200',
+      credentials: true,
+    }),
+  );
 
   await app.listen(8000);
 }
